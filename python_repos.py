d9253767ce9154174045236a7422eaca46cafa62
aaronzh <aaron@burnsred.com.au>
2019-03-15 17:26:57 +1100

    temp

--- python_repos.py
+++ python_repos.py
@@ -16,10 +16,16 @@ print('total repositories: ', response_dict['total_count'])
 print('repositories returned: ', len(response_dict['items']))
 print("\nSelected information about each repository:")
 repo_dicts = response_dict['items']
-names, stars = [], []
+names, plot_dicts = [], []
+i=0
 for repo_dict in repo_dicts:
+    print(i, repo_dict['stargazers_count'], repo_dict['description'], repo_dict)
+    i += 1
     names.append(repo_dict['name'])
-    stars.append(repo_dict['stargazers_count'])
+    if not repo_dict['description']:
+        repo_dict['description'] = ''
+    plot_dicts.append({'value': repo_dict['stargazers_count'], 'label': repo_dict['description']})
+
 
 # Make visualization.
 my_style = LS('#333366', base_style=LCS)
@@ -35,6 +41,6 @@ my_config.value_formatter = lambda x: '{:,}'.format(x)
 chart = pygal.Bar(my_config, style=my_style)
 chart.title = 'Most-Starred Python Projects on GitHub'
 chart.x_labels = names
-chart.add('', stars)
+chart.add('', plot_dicts)
 chart.render_to_file('python_repos.svg')
 
