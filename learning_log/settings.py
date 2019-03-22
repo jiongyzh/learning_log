055e9f10482783a6a3858c2da237239cc6d9520e
aaronzh <aaron@burnsred.com.au>
2019-03-22 12:43:22 +1100

    add abs directory for static_root

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -141,7 +141,7 @@ if os.getcwd() == '/app':
     ALLOWED_HOSTS = ['*']
     # Static asset configuration
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = 'staticfiles'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'static'),
     )
